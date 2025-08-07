f6df85057e648a7d02b3290c803610f2f3ee7255
raylander085 <o.raylan@escola.pr.gov.br>
2025-08-07 10:50:05 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,28 @@
-
+const perguntas = [
+    {
+    categoria: "[matemática]",
+    pergunta: "[quantos dias tem na semana?]",
+    resposta: "[7]"
+    },
+    {
+    categoria: "[História]",
+    pergunta: "[Qual evento derivou diretamente da queda da Bastilha em 1789?]",
+    resposta: "[A Revolução Francesa se intensificou, marcando o fim do absolutismo na França.]"
+    },
+    {
+    categoria: "[Biologia]",
+    pergunta: "[O que pode derivar de uma mutação genética em uma espécie?]",
+    resposta: "[Pode surgir uma nova característica, que será vantajosa, neutra ou prejudicial à sobrevivência da e]"
+    },
+    {
+    categoria: "[Linguística]",
+    pergunta: "[A palavra felicidade deriva de qual palavra base?]",
+    resposta: "[Deriva da palavra feliz]"
+    },
+    {
+    categoria: "[Geografia]",
+    pergunta: "[O que pode derivar do desmatamento da Amazonia?]",
+    resposta: "[perda de biodiversidade e impactos em comunidades locais.]"
+    }
+    ];
+    
